61e3de1186e9948f1155f952c77130e9cda4fbe5
Leonardo Santella <leocaracas2010@gmail.com>
2017-04-20 23:41:38 -0400

    D AC

--- Codeforces/Contests/2015-Facebook-HackerCup/Round 1/D.cpp
+++ Codeforces/Contests/2015-Facebook-HackerCup/Round 1/D.cpp
@@ -1,17 +1,50 @@
 #include <bits/stdc++.h>
-#define N 2000007
+#define MAXN 200005
+#define MAXC 12
 using namespace std;
 
-int t, n;
-int p[N], g[N];
+typedef vector<int> vi;
+int memo[MAXN][MAXC], n, t;
+vi g[MAXN];
+
+int dp(int cur, int col)
+{
+    int &best = memo[cur][col];
+    if (best != -1) return best;
+    int ans = col;
+    
+    vi &child = g[cur];
+    for (int i = 0; i < child.size(); ++i){
+        int aux = 0x3f3f3f3f;
+        for (int j = 1; j < MAXC; ++j){
+            if (j == col) continue;
+            aux = min(dp(child[i], j), aux);
+        }
+        ans += aux;
+    }
+    return best = ans;
+}
 
 int main()
 {
+    //freopen("in.txt", "r", stdin);
+    int a;
     scanf("%d", &t);
-    for(int i = 1; i <= t; ++i)
+    for (int i = 1; i <= t; ++i)
     {
         scanf("%d", &n);
-        for (int j = 0; j < n; ++j)
-            scanf("%d", &p[j]);
+        for (int j = 0; j <= n; ++j)
+            g[j].clear();
+        for (int j = 1; j <= n; ++j)
+        {
+            scanf("%d", &a);
+            g[a].push_back(j);
+        }
+        memset(memo, -1, sizeof(memo));
+        int ans = 0x3f3f3f3f;
+        for (int j = 1; j < MAXC; ++j)
+            ans = min(dp(1,j), ans);
+        printf("Case #%d: %d\n", i, ans);
     }
+    return 0;
 }
