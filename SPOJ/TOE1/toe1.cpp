6cb68b0982bf14a7548630dcdf01e0e8b367f9d9
Leonardo Santella <leocaracas2010@gmail.com>
2017-08-04 08:32:40 -0400

    Training SAL 1

--- SPOJ/TOE1/toe1.cpp
+++ SPOJ/TOE1/toe1.cpp
@@ -1,61 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-set<string> possibles;
-string g;
-int xs, os, total;
-bool mark[10];
+set<string> gset;
 
-bool play(char l, int turn){
-    if (turn == total)
-        return true;
-    else if ((l == 'X' && !xs) || (l == 'O' && !os && xs))
-        return false;
-    else{
-        for (int i = 0; i < 9;i++){
-            //printf("%c", grid[i]);
-            if (g[i] == l){
-                g[i] = '.';
-                if (l == 'X') 
-                    xs--;
-                else
-                    os--;
-                return play((l == 'X')? 'O': 'X', turn+1);
-            }   
-        }
+bool win(string game){
+    string x = "XXX", o = "OOO";
+    for (int i = 0; i < 9; i += 3){
+        if (game.substr(i,3) == x || game.substr(i,3) == o)
+            return true;
     }
+    for (int i = 0; i < 3; i++){
+        if (game[i] == game[i+3] && game[i+3] == game[i+6] && game [i] != '.')
+            return true;
+    }
+    if (game[0] == game[4] && game[4] == game[8] && game[4] != '.')
+        return true;
+    if (game[2] == game[4] && game[4] == game[8] && game[4] != '.')
+        return true;
     return false;
 }
 
+
+void generate(string &game, bool turn){
+    if (gset.count(game))
+        return;
+    gset.insert(game);
+    if (win(game)) return;
+    for(int i = 0; i < 9; i++){
+        if (game[i] == '.'){
+            game[i] = turn ? 'X': 'O';
+            generate(game, !turn);
+            game[i] = '.';
+        }
+    }
+}
+
 int main() {
-    freopen("in", "r", stdin);
+    //freopen("in", "r", stdin);
     int n;
     scanf("%d\n", &n);
     char ch;
     int aux;
+    string g = ".........";
+    generate(g, 1);
     while(n--){
         g = "";
-        xs = os = 0;
         for (int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 scanf("%c", &ch);
                 g.push_back(ch);
-                if (ch == 'X') 
-                    xs++;
-                else if (ch == 'O')
-                    os++;
             }
             scanf("\n");
         }
-        aux = os;
-        aux = xs;
-        total = os+xs;
-        aux = total;
-        if (play('X', 0))
-            printf("yes\n");
-        else
-            printf("no\n");
         scanf("\n");
+        printf("%s\n", gset.count(g) ? "yes" : "no");
     }
     return 0;
 }
