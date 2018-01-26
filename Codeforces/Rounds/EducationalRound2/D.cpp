54bd0cad96cde35aa471160c3eeafa18bd6d0f67
Leonardo Santella <leocaracas2010@gmail.com>
2018-01-25 20:21:31 -0400

    Updates

--- Codeforces/Rounds/EducationalRound2/D.cpp
+++ Codeforces/Rounds/EducationalRound2/D.cpp
@@ -7,7 +7,10 @@ int main(){
     long long int x1,x2,y1,y2,r1,r2;
     long double alpha1, alpha2, aux, d, dsqr, p1x, t1, s1, t2, s2, ans1, ans2;
     cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-    dsqr = sqr(x1-x2) + sqr(y1-y2);
+    x2 -= x1;
+    y2 -= y1;
+    x1 = y1 = 0;
+    dsqr = sqr(x2) + sqr(y2);
     d = sqrt(dsqr);
     if (dsqr >= sqr(r1+r2)){
         printf("%.20Lf\n", 0.0L);
@@ -21,16 +24,21 @@ int main(){
         printf("%.20Lf\n", PI*(long double)sqr(r1));
         return 0;
     }
-    p1x = (dsqr + sqr(r2) - sqr(r1)) / (2*d);
+    if (x1 == x2 && y1 == y2 && r1 == r2){
+        printf("%.20Lf\n", PI*(long double)sqr(r1));
+        return 0;
+    }
+    p1x = (dsqr + sqr(r1) - sqr(r2)) / (2*d);
     aux = p1x/r1;
     alpha1 = acos(aux);
     t1 = sqr(r1)*sin(alpha1)*cos(alpha1);
-    s1 = alpha1*sqr(r1);
+    s1 = sqr(r1)*alpha1;
     ans1 = s1 - t1;
-    alpha2 = acos(fabs(d-p1x)/r2);
+    p1x = (dsqr + sqr(r2) - sqr(r1)) / (2*d);
+    alpha2 = acos(p1x/r2);
     t2 = sqr(r2)*sin(alpha2)*cos(alpha2);
     s2 = alpha2*sqr(r2);
-    ans2 = s2 - t2;
+    ans2 = s2-t2;
     printf("%.20Lf\n", ans2+ans1);
     return 0;
 }
--- Library/Graphs/mst.cpp
+++ Library/Graphs/mst.cpp
@@ -1,1 +1,53 @@
-mst.cpp
+#include <bits/stdc++.h>
+#define N 100000
+using namespace std;
+
+typedef pair<int, int> ii;
+
+
+int p[N], dsu_size;
+vector<int, ii> g, ans; // edges
+
+int dsu_find(int x){
+    if (p[x] == x)
+        return x;
+    return p[x] = find(p[x]);
+}
+
+void dsu_union(int x, int y){
+    int rootx = dsu_find(x);
+    int rooty = dsu_find(y);
+    if (rootx != rooty){
+        p[rooty] = rootx;
+        dsu_size++;
+    }
+}
+
+void dsu_init(int n){
+    dsu_size = n;
+    for(int i = 0; i < n; i++)
+        p[i] = i;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    dsu_init(n);
+    g.assign(n+1);
+    for (int i = 0; i < n; i++){
+        scanf("%d %d %d", &g[i].second.first, &g[i].second.second, &g[i].first) // x y weight
+    }
+    sort(g.begin(), g.end());
+    int mst_weight = 0;
+    for (int i = 0; i < n; i++){
+        int a = g[i].second.first, b = g[i].second.second, c = g[i].first;
+        if (dsu_find(a) != dsu_find(b)){
+            ans.push_back(c,ii(a,b));
+            mst_weight += c;
+        }
+        if (ans.size() == n-1) break; //
+    }
+    for (int i = 0; i < ans.size(); i++)
+        printf("%d %d %d\n", ans[i].second.first, ans[i].second.second, ans[i].first);
+    return 0;
+}
